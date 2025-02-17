f6b143d5a796dcfa4e8f91f15dacdf2ef4d8ddc0
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-02-17 14:57:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",function () {
+	let button = document.getElementById('btn');
+	
+	let outputText = document.getElementById('output');
+	button.addEventListener('click',function() {
+	
+	let inputText = document.getElementById('text').value.trim();
+	let delayInput = parseInt(document.getElementById('delay').value,10);
+		//outputText.innerHTML = '';
+
+		if (isNaN(delayInput) || delayInput < 0) {
+            outputText.innerHTML = 'Please enter a valid delay in milliseconds.';
+            return;
+        }
+		
+		function promise(){
+			return new Promise ((resolve)=>{
+				setTimeout(() => {
+					resolve(inputText);
+				},delayInput);
+			});
+		}
+		 async function displayMessage() {
+            let message = await promise();
+            outputText.innerHTML = message;
+        }
+		displayMessage();
+	});
+	
+})
